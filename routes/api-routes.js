601d6a55d56439642805de5d739a7f4ccdd2f33b
unknown <wroehrman@yahoo.com>
2019-01-01 18:35:29 -0600

    updates

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -64,7 +64,7 @@ module.exports = function (app) {
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
     });
     // alert("Email has been sent!");
-    res.redirect('/index');
+    res.redirect('/');
 
 
 
